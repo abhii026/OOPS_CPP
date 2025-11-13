8097703cce1169ff4468b7b8568293ef96d0406f
Abhishek Singh <iabhii026@gmail.com>
2025-11-13 10:37:50 +0530

    New file add

--- File_Handling/binary/l1.cpp
+++ File_Handling/binary/l1.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 int main()
 {
-    fstream file("data.bin", ios::out);
-    
+    fstream file;
+    file.open("data.bin", ios::out | ios::in | ios::binary | ios::app);
+    int a = 10, b;
+    file.write(reinterpret_cast<char *>(&a), 4);
+    file.seekg(0, ios::beg);
+    file.read(reinterpret_cast<char *>(&b), 4);
+    cout << b;
     return 0;
 }
