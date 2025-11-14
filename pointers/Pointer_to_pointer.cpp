4b5fa3866595d5f2ea58e4d358ba635564f9a922
Abhishek Singh <iabhii026@gmail.com>
2025-11-14 10:55:59 +0530

    New file

---
+++ pointers/Pointer_to_pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 10;
+    int *p = &a;  // p stores address of a
+    int **q = &p; // q stores address of p
+    cout << "Address of a: " << p << endl;
+    cout << "Value of a(*p): " << *p << endl;
+    cout << "Value inside (*p) **q : " << **q << endl;
+    cout << "Address of p: " << q << endl;
+
+    return 0;
+}
