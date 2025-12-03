60082fc7752f6f5bdb57ef0916b70632b96a6fd6
Abhishek Singh <iabhii026@gmail.com>
2025-12-03 13:53:10 +0530

    Updated Ambiguity file

--- Inheritance/Ambiguity.cpp
+++ Inheritance/Ambiguity.cpp
@@ -12,10 +12,17 @@ class B{
         cout<<"B's Print!"<<endl;
     }
 };
-class C: public A, public B{};
+class C: public A, public B{
+    public:
+    void print(){
+        A::print();
+        B::print();
+    }
+};
 int main(){
     C objC;
-    objC.A::print();
-    objC.B::print();
+    // objC.A::print();
+    // objC.B::print();
+    objC.print();
     return 0;
 }
