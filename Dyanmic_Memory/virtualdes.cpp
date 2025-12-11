fe8d52ed7d4b67373fa2d6071fac5488f4f097f1
Abhishek Singh <iabhii026@gmail.com>
2025-12-11 11:37:59 +0530

    Updated Virtual destructor

--- Dyanmic_Memory/virtualdes.cpp
+++ Dyanmic_Memory/virtualdes.cpp
@@ -10,14 +10,23 @@ public:
 };
 class B : public A
 {
+    int *p;
+
 public:
+    B()
+    {
+        p = new int[10];
+    }
     ~B()
     {
+        delete[] p;
         cout << "dtor of class B" << endl;
     }
 };
 int main()
 {
+
+    
 
     A *ptr_A = new B();
     delete ptr_A;
