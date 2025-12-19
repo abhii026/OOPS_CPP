fe5b9b8161302db4e05d9cc5896d8885b0fbd741
Abhishek Singh <iabhii026@gmail.com>
2025-12-19 10:43:07 +0530

    Added Age eligible using try catch

---
+++ Exception_Handling/ageprblm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    try
+    {
+        cout << "Enter your age: ";
+        cin >> age;
+        if (age < 0)
+        {
+            throw "Age can not be Negative.";
+        }
+        else if(age<18){
+            throw "Under age.";
+        }
+        cout<<"Eligible";
+    }
+    catch (const char *e)
+    {
+        cout << "Caught an exception: " << e << endl;
+    }
+    cout<<"Done.";
+    return 0;
+}
