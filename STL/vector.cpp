014a4e1c03372c57bc77055e5b71f63fc72c5f5d
Abhishek Singh <iabhii026@gmail.com>
2025-12-27 13:52:40 +0530

    Added vector

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -3,16 +3,20 @@
 using namespace std;
 int main()
 {
-    vector<int> vec;
-    cout<<vec.size()<<endl;
-    cout<<sizeof(vec)<<endl;
-    // vec.push_back(10);
-    // vec.push_back(20);
-    // vec.push_back(30);
-    // for (int i = vec.begin(); i != vec.end(); ++i)
-    // {
-    //     cout << *i << " ";
-    // }
-    // cout << endl;
+    vector<int> vec(6);
+    cout << vec.size() << endl;  // prints 6
+    cout << sizeof(vec) << endl; // prints 12
+
+    cout << "Enter 6 elements: " << endl;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        cin >> vec[i];
+
+    }
+    for (int i = 0; i < vec.size(); i++)
+    {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
